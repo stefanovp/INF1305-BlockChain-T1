75cccad00bc149fb63379ca07f08449a89ce66c9
Stefano <stefano.viper@gmail.com>
2020-10-29 20:06:46 -0300

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,5 @@
 console.log("teste 0");
-const contractMetadata = $.getJSON('./javascript/contractMetadata.json').responseJSON;
+const contractMetadata = await $.getJSON('./javascript/contractMetadata.json');
 const contract = web3.eth.contract(contractMetadata.abi, contractMetadata.address);
 console.log(contractMetadata);
 console.log(contract);
